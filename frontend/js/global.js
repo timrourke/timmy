ac1bd2163104c82b534a222d30364bb4fb507065
Tim Rourke <tim@timrourke.com>
2015-05-24 10:02:07 -0500

    worked out issues with static layout using flexbox.

--- frontend/js/global.js
+++ frontend/js/global.js
@@ -1,29 +1,45 @@
 // "use strict";
 
 var socket = io(window.location.host);
+
 $('#chatForm').submit(function() {
-	socket.emit('chat message', $('#m').val());
-	$('#m').val('');
+  if ( $('#m').val() ) {
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+  }
 	return false;
 });
+
 socket.on('connect', function() {
 
 });
 
+function appendMessage(item) {
+  $('#messages').append(item);
+  $('.right').animate({
+    scrollTop: $('.right').height()
+  }, 'slow');
+  return false;
+}
+
 socket.on('join message', function(msg) {
-  $('#messages').append($('<li class="chatMessage">').html('<span class="systemMessage"><em>' + msg.name + ' has joined ' + msg.room + '</span>'));
+  var item = $('<li class="chatMessage">').html('<span class="systemMessage"><em>' + msg.name + ' has joined ' + msg.room + '</span>');
+  appendMessage(item);
 });
 
 socket.on('chat message', function(msg) {
-	$('#messages').append($('<li class="chatMessage">').html('<span class="chatUser">' + msg.name + ':</span>' + msg.message));
+  var item = $('<li class="chatMessage">').html('<span class="chatUser">' + msg.name + ':</span>' + msg.message);
+  appendMessage(item);
 });
 
 socket.on('user connected', function(msg) {
-  $('#messages').append($('<li class="chatMessage">').html('<span class="systemMessage"><em>User connected: </em>' + msg.name + '</span>'));
+  var item = $('<li class="chatMessage">').html('<span class="systemMessage"><em>User connected: </em>' + msg.name + '</span>');
+  appendMessage(item);
 });
 
 socket.on('user disconnected', function(msg) {
-  $('#messages').append($('<li class="chatMessage">').html('<span class="systemMessage"><em>User disconnected: </em>' + msg.name + '</span>'));
+  var item = $('<li class="chatMessage">').html('<span class="systemMessage"><em>User disconnected: </em>' + msg.name + '</span>');
+  appendMessage(item);
 });
 
 socket.on('user list', function(users) {
@@ -33,3 +49,4 @@ socket.on('user list', function(users) {
     $('#users').append($('<li>').text(users[user]));
   }
 });
+
